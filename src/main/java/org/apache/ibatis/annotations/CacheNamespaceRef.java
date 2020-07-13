b1abe1daf878644e3806f355b0dbc95692f057c6
sunjinwei <sunjinwei5828@163.com>
2020-07-14 07:49:01 +0800

    --test

--- src/main/java/org/apache/ibatis/annotations/CacheNamespaceRef.java
+++ src/main/java/org/apache/ibatis/annotations/CacheNamespaceRef.java
@@ -24,6 +24,10 @@ import java.lang.annotation.Target;
 /**
  * The annotation that reference a cache.
  *
+ *
+ *
+ *
+ *
  * <p>If you use this annotation, should be specified either {@link #value()} or {@link #name()} attribute.
  *
  * <p>
